332f07a26fff8f9164cd4bc55362d7237abaebfc
margolanier <mplanier@live.unc.edu>
2014-04-29 18:47:40 -0400

    Initial commit

---
+++ js/goddardInsta.js
@@ -0,0 +1,37 @@
+// JavaScript Document
+
+
+//Use this url below to get your access token
+//https://instagram.com/oauth/authorize/?display=touch&client_id=f0ce982c58da406ca8977aa802411542&redirect_uri=http://margolanier.com/static/instagram&response_type=token 
+
+//if you need a user id for yourself or someone else use:
+//http://jelled.com/instagram/lookup-user-id
+	
+						
+$(function() {
+	
+	var apiurl = "https://api.instagram.com/v1/users/3808579/media/recent?access_token=345652432.f0ce982.26510f81a202427491eeb027670e4f1d&callback=?"
+	var access_token = location.hash.split('=')[1];
+	var html = ""
+	
+	$.ajax({
+		type: "GET",
+		dataType: "json",
+		cache: false,
+		url: apiurl,
+		success: parseData
+	});	
+	
+	function parseData(json){
+		console.log(json);
+		
+		$.each(json.data,function(i,data){
+			html += '<img src ="' + data.images.low_resolution.url + '">'
+		});
+		
+		console.log(html);
+		$(".results").append(html);
+		
+	}
+	
+});
